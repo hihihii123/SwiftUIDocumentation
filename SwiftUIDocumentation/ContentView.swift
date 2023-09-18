25770be9ae9ad7d23f11610c639f4372762b03ca
Ang Sen Luen Matthias <ang_sen_luen_matthias@s2023.ssts.edu.sg>
2023-09-18 20:13:34 +0800

    just some placeholder code :)

--- SwiftUIDocumentation/ContentView.swift
+++ SwiftUIDocumentation/ContentView.swift
@@ -6,14 +6,49 @@
 //
 
 import SwiftUI
+import SwiftPersistence
 
 struct ContentView: View {
+    @Persistent("now", store: .appStorage) var now = ["1"]
+    @Persistent("time", store: .appStorage) var times = 0
+    @State var list = ""
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Placeholder code")
+            TextField("Input here", text: $list)
+            Button {
+                times += 1
+                list = "\(times). \(list)"
+                now.append(list)
+                list = ""
+            } label: {
+                Text("Confirm")
+            }
+            List {
+                ForEach(now, id: \.self) {
+                    value in
+                    Text(value)
+                        /*.swipeActions {
+                            Button{
+                                now.remove(at: )
+                            } label: {
+                                HStack {
+                                    Text("Delete")
+                                }
+                            }
+                        }
+                         */
+                }
+                Button(role: .destructive) {
+                    now.removeAll()
+                    times = 0
+                } label: {
+                    Text("Remove all")
+                }
+            }
         }
         .padding()
     }
@@ -22,3 +57,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+

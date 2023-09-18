dfc24dbedf09fb76dad9d82e8168ed5d6dbaba7d
Ang Sen Luen Matthias <ang_sen_luen_matthias@s2023.ssts.edu.sg>
2023-09-18 18:27:57 +0800

    Initial Commit

---
+++ SwiftUIDocumentation/SwiftUIDocumentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDocumentationApp.swift
+//  SwiftUIDocumentation
+//
+//  Created by Ang Sen Luen Matthias on 18/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDocumentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

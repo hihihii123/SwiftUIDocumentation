558c5eff31a0c1304173112caae5380f23786afc
Ang Sen Luen Matthias <ang_sen_luen_matthias@s2023.ssts.edu.sg>
2023-10-19 12:01:18 +0800

    minor edits

---
+++ Lesson1.swift
@@ -0,0 +1,18 @@
+//
+//  Lesson1.swift
+//  SwiftUIDocumentation
+//
+//  Created by Ang Sen Luen Matthias on 19/10/23.
+//
+
+import SwiftUI
+
+struct Lesson1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Lesson1()
+}
